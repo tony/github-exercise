0d7b9ce42c0075f4bd7ce3210508e93cef4ded6b
Tony Narlock <tony@git-pull.com>
2014-02-21 08:06:26 +0800

    Initial commit

---
+++ _directions/index.js
@@ -0,0 +1,33 @@
+
+
+// Sample for handling the rendering
+//
+// var emitter = new EventEmitter();
+//
+// function loadRepos(user){
+//   emitter.emit('data.repos', repos);
+//   repos.forEach(function(repo){
+//     loadBranches(repo);
+//     loadCollaborators(repo);
+//   });
+//   emitter.on('data.branches.*', renderBranches);
+//   emitter.on('data.collaborators.*', renderCollaborators);
+// }
+//
+// function loadBranches(repo){
+//   emitter.emit('data.branches.jquery', branches);
+// }
+//
+// function loadCollaborators(repo){
+//   emitter.emit('data.collaborators.jquery', collaborators);
+// }
+//
+// // render repositories data
+// function renderBranches(data, type){ }
+//
+// // render branches data
+// function renderBranches(data, type){ }
+//
+// // render collaborators data
+// function renderCollaborators(data, type){ }
+
